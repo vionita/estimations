2ab9f7caf0e0fc071ec029493976f02109222eb0
Valentin IONITA <valentin.ionita87@gmail.com>
2015-11-05 22:44:14 +0100

    iova:refine intro

---
+++ js/charts.js
@@ -0,0 +1,89 @@
+var charts = {
+
+    chaos2004 : {
+        "type": "pie",
+        "balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
+        "labelRadius": 15,
+        "colors": [
+            "#696969",
+            "#696969",
+            "#008000",
+            "#a7a737",
+            "#86a965",
+            "#8aabb0",
+            "#69c8ff",
+            "#cfd27e",
+            "#9d9888",
+            "#916b8a",
+            "#724887",
+            "#7256bc"
+        ],
+        "outlineThickness": 1,
+        "titleField": "category",
+        "valueField": "column-1",
+        "handDrawScatter": 4,
+        "handDrawThickness": 3,
+        "theme": "black",
+        "allLabels": [],
+        "balloon": {},
+        "titles": [],
+        "dataProvider": [
+            {
+                "category": "failed",
+                "column-1": "18"
+            },
+            {
+                "category": "overbudget",
+                "column-1": "54"
+            },
+            {
+                "category": "ontime/onbudget",
+                "column-1": "28"
+            }
+        ]
+    },
+
+    chaos2014: {
+        "type": "pie",
+        "balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
+        "labelRadius": 15,
+        "colors": [
+            "#696969",
+            "#696969",
+            "#008000",
+            "#a7a737",
+            "#86a965",
+            "#8aabb0",
+            "#69c8ff",
+            "#cfd27e",
+            "#9d9888",
+            "#916b8a",
+            "#724887",
+            "#7256bc"
+        ],
+        "outlineThickness": 1,
+        "titleField": "category",
+        "valueField": "column-1",
+        "handDrawScatter": 4,
+        "handDrawThickness": 3,
+        "theme": "black",
+        "allLabels": [],
+        "balloon": {},
+        "titles": [],
+        "dataProvider": [
+            {
+                "category": "failed",
+                "column-1": "18"
+            },
+            {
+                "category": "overbudget",
+                "column-1": "43"
+            },
+            {
+                "category": "ontime/onbudget",
+                "column-1": "39"
+            }
+        ]
+    }
+
+};
